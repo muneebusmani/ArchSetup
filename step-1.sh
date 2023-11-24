d64146c2928ff97f76b55791fd0c4cc0fd724abf
muneeb <muneebusmani8355@gmail.com>
2023-11-25 02:16:42 +0500

    Arch Linux Setup Scripts

---
+++ step-1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd ~ 
+sudo pacman -Syyuu git base-devel
+pacman -S --needed git base-devel 
+git clone https://aur.archlinux.org/yay.git
+cd yay 
+makepkg -si
+
+cd ~ 
+yay -Syyuu zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+yay -S --noconfirm zsh-theme-powerlevel10k-git 
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+cd ~
+git clone https://aur.archlinux.org/snapd.git 
+cd snapd 
+makepkg -si 
+sudo systemctl enable --now snapd.socket && sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install snap-store
+sudo pacman -S flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo reboot
---
+++ step-2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ~
+sudo systemctl enable NetworkManager --now 
+sudo systemctl enable bluetooth --now
+yay -Syyuu vim neovim gnome gnome-tweaks microsoft-edge-stable-bin freedownloadmanager visual-studio-code-bin php mysql mysql-workbench phpmyadmin composer mssql-server dotnet-sdk nodejs npm nvm python go dart-sdk-dev flutter mongodb
+composer global require laravel/installer
+dotnet tool install dotnet-ef
+npm i -g @angular/cli
+npm i -g typescript
+npm i -g express
